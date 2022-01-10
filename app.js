1b3434b03a52fcc8a7955ad3af4fb94f81fb1d82
Latief Irfansyah <latifirfansyah@gmail.com>
2022-01-10 07:09:38 +0700

    read files

--- app.js
+++ app.js
@@ -6,5 +6,16 @@
 
 // message('me or')
 
-const{users, nums} = require('./users');
-console.log(nums);
+// const{users, nums} = require('./users');
+// console.log(nums);
+
+const fs = require('fs')
+//read file
+fs.readFile('./note.txt',(err, data) => {
+    if(err){
+        console.log(err)
+    }else {
+        console.log(data.toString())
+    }
+})
+//write
