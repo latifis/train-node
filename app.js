87bc38e480084cabe930342c7e46fb6c9f04d81b
Latief Irfansyah <latifirfansyah@gmail.com>
2022-01-10 08:38:11 +0700

    buffer and stream

--- app.js
+++ app.js
@@ -63,15 +63,31 @@ const fs = require('fs')
 //     console.log('The folder already deleted')
 // }
 
-//delete file
-if(fs.existsSync('./ckmobile/note.txt')){
-    fs.unlink('./ckmobile/note.txt', (err, data) => {
-        if(err){
-            console.log(err)
-        }else {
-            console.log('the file is deleted')
-        }
-    })
-}else{
-    console.log('The file already deleted')
-}
+// //delete file
+// if(fs.existsSync('./ckmobile/note.txt')){
+//     fs.unlink('./ckmobile/note.txt', (err, data) => {
+//         if(err){
+//             console.log(err)
+//         }else {
+//             console.log('the file is deleted')
+//         }
+//     })
+// }else{
+//     console.log('The file already deleted')
+// }
+
+const readStream = fs.createReadStream('largetext.txt', {encoding: 'utf-8'});
+
+const writeStream = fs.createWriteStream('writeStream.txt');
+
+// readStream.on('data', chunk => {
+//     console.log('##### new chunk #####\n');
+//     console.log(chunk.toString())
+
+//     writeStream.write('\n ##### chunk #####\n')
+//     writeStream.write(chunk)
+    
+// })
+
+//stream.pipe()
+readStream.pipe(writeStream)
