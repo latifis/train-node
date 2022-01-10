5a339bb947decfc648e6ac150352b1ae1f72bba3
Latief Irfansyah <latifirfansyah@gmail.com>
2022-01-10 18:52:29 +0700

    return html

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 const http = require('http');
+const fs = require('fs');
 // const { hostname } = require('os');
 const port = 3000;
 
 const  server = http.createServer((req,res)=>{
     //if text
-    // res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/html');
     // res.write('Welcome to ckmobile');
+    fs.readFile('./view/index.html',(err,data)=>{
+        if(err){
+            console.log(err)
+            res.end();
+        }else{
+            res.write(data);
+            res.end();
+        }
+    })
 
     //if html
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<body class="app"></body>');
-    res.write('<h3>Welcome to ckmobile</h3>');
-    res.write('<p>this is a Node.Js train</p>');
-    res.end();
+    // res.setHeader('Content-Type', 'text/html');
+    // res.write('<body class="app"></body>');
+    // res.write('<h3>Welcome to ckmobile</h3>');
+    // res.write('<p>this is a Node.Js train</p>');
+    
 });
 
 //server.listen(port, hostname, ()
