fdc512f106dfd255f30817517e594b81770370be
Latief Irfansyah <latifirfansyah@gmail.com>
2022-01-10 10:31:16 +0700

    res method

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ const http = require('http');
 const port = 3000;
 
 const  server = http.createServer((req,res)=>{
-    console.log('request url', req.url);
-    console.log('request method', req.method)
+    //if text
+    // res.setHeader('Content-Type', 'text/plain');
+    // res.write('Welcome to ckmobile');
+
+    //if html
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<body class="app"></body>');
+    res.write('<h3>Welcome to ckmobile</h3>');
+    res.write('<p>this is a Node.Js train</p>');
+    res.end();
 });
 
 //server.listen(port, hostname, ()
