1b8e0055ecbc384331d64a5dfb699203779a5328
Latief Irfansyah <latifirfansyah@gmail.com>
2022-01-10 07:00:13 +0700

    add modules

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-console.log('ckmobel')
+// console.log('ckmobel')
 
-const message = (name) =>{
-    console.log(`${name} is good`)
-}
+// const message = (name) =>{
+//     console.log(`${name} is good`)
+// }
 
-message('me or')
+// message('me or')
+
+const importedStuff = require('./users');
+console.log(importedStuff);
