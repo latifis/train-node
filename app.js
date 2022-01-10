974f97909ad059581d62209ff9dd3728d62138e9
Latief Irfansyah <latifirfansyah@gmail.com>
2022-01-10 07:49:42 +0700

    create folder

--- app.js
+++ app.js
@@ -28,11 +28,24 @@ const fs = require('fs')
 //     }
 // })
 
-//append files
-fs.appendFile('./ckmobile/note.txt', 'A new message \r\n', (err, data) => {
-    if(err){
-        console.log(err)
-    }else {
-        console.log('A new message create')
-    }
-})
+// //append files
+// fs.appendFile('./ckmobile/note.txt', 'A new message \r\n', (err, data) => {
+//     if(err){
+//         console.log(err)
+//     }else {
+//         console.log('A new message create')
+//     }
+// })
+
+//create folder
+if(!fs.existsSync('newfolder')){
+    fs.mkdir('newfolder', (err, data) => {
+        if(err){
+            console.log(err)
+        }else {
+            console.log('A new folder create')
+        }
+    })
+}else{
+    console.log('The folder already exist')
+}
